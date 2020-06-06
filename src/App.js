506ffa3a8d52afc540dbe44bc5468ce049e40b0d
Dmitry <dmitry.pashkurov@mail.ru>
2020-06-06 10:46:25 +0300

    Начал вести историю

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import logotip from './logo.jpeg'
+import Header from "./Header";
+import Nav from './Nav'
+import Profile from './Profile'
+
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Nav />
+            <Profile />
+        </div>
+
+    );
+}
+
+
+export default App;
+
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.pinimg.com/originals/8a/ac/df/8aacdf3e55fabf44242aed7a86b22496.png' />
+        </header >
+    )
+}
+
+export default Header;
