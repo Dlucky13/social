f091e6f13a2314e94416f151c96f09dd1029dd69
Dmitry <dmitry.pashkurov@mail.ru>
2020-06-19 19:34:37 +0300

    подключает axios и получает пользователей с сервера

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,17 @@
 import React from "react";
 import styles from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/img/avatar_default.jpg'
 
 let Users = (props) => {
 
     if (props.usersData.length === 0) {
-        props.setUsers([
-            {
-                id: 1, name: 'Dmitry', follow: true, status: 'just happy', location: 'Saint-Petersburg',
-                ava: 'https://i1.sndcdn.com/avatars-000323700456-wu94n2-t500x500.jpg'
-            },
-            {
-                id: 2, name: 'Kostya', follow: false, status: 'work hard', location: 'Moscow',
-                ava: 'https://i1.sndcdn.com/avatars-000323700456-wu94n2-t500x500.jpg'
-            },
-            {
-                id: 3, name: 'Sasha', follow: false, status: '...', location: 'Kiev',
-                ava: 'https://i1.sndcdn.com/avatars-000323700456-wu94n2-t500x500.jpg'
-            },
-            {
-                id: 4, name: 'Maksim', follow: true, status: 'gl hf', location: 'Amsterdam',
-                ava: 'https://i1.sndcdn.com/avatars-000323700456-wu94n2-t500x500.jpg'
-            },
-        ])
-    }
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then (response => {
+                props.setUsers(response.data.items)
+            }
+           )
+      }
 
 
     return (
@@ -30,7 +19,7 @@ let Users = (props) => {
             {props.usersData.map(user =>
                 <div key={user.id} className={styles.user_wrapper}>
                     <div className={styles.user_status}>
-                        <img src={user.ava} className={styles.ava}></img>
+                        <img src={user.photos.small = userPhoto} className={styles.ava}></img>
                         {user.follow ? <button className={styles.btn} onClick={ ( ) => {props.unfollow(user.id)}}>Unfollow</button>
                             : <button className={styles.btn} onClick={ () => {props.follow(user.id)}}>Follow</button>}
                     </div>
@@ -39,7 +28,7 @@ let Users = (props) => {
                             <div>{user.name}</div>
                             <div>{user.status}</div>
                         </div>
-                        <div>{user.location}</div>
+                        <div>{'user.location'}</div>
                     </div>
                 </div>
 
